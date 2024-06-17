367af4a9b41c1d55927e0bfd36bbfe824cc58a6c
kuei <cary0522@gmail.com>
2024-06-17 15:22:36 +0800

    GET page

--- js/partnerspage.js
+++ js/partnerspage.js
@@ -23,35 +23,100 @@ $('.footer-page').load('footer.html');
 // }))
 
 function getInfo() {
+    let partner_name = localStorage.getItem('partnerName');
     let partner_appearance = localStorage.getItem('partnerAppearance');
+    let partnerAppearanceArr = JSON.parse(partner_appearance);
+    let partner_trait = localStorage.getItem('partnerTrait');
+    let partnerTraitArr = JSON.parse(partner_trait);
+
+    let partnerName = document.getElementById('partner-name');
+    partnerName.innerText = partner_name;
+    let interactivePartnerName = document.getElementById('interactive-partner-name')
+    interactivePartnerName.innerText = partner_name;
+
+    let info = document.getElementById('info');
+    info.innerHTML = ` 
+    <p class='fs-5 m-0'> ${partner_name} ，是一位${partnerAppearanceArr[0]}風格 、 具有${partnerAppearanceArr[1]} 、 ${partnerAppearanceArr[2]}的${partnerAppearanceArr[3]}寵物！
+        個性${partnerTraitArr[0]} 、 ${partnerTraitArr[1]} 、 ${partnerTraitArr[2]} ，而且喜愛${partnerTraitArr[3]}！ </p>
+    `
+
+    function Partner(appearance, trait) {
+        this.appearance = appearance;
+        this.trait = trait
+    }
+
+    let AiPartner = new Partner(partnerAppearanceArr, partnerTraitArr);
+    console.log(AiPartner.appearance);
+    console.log(AiPartner.trait);
+
     fetch('http://127.0.0.1/AIpartners.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: partner_appearance
+        body: JSON.stringify(AiPartner)
     }).then((response) => {
-        console.log('success');
         return response.json();
     }).then((res) => {
-        console.log(res);
-        // let partner_appearance = localStorage.getItem('partnerAppearance');
-        // let partnerAppearanceArr = JSON.parse(partner_appearance);
-
         let photo = document.getElementById('photo');
         let interactivePartnerPhoto = document.getElementById('interactive-partner-photo')
 
-        photo.innerHTML = `<img src='data:image/png;base64, ${res}' alt=''>`
+        photo.innerHTML = `<img src='data:image/png;base64, ${res['image']}' alt=''>`
         interactivePartnerPhoto.innerHTML =
-            `<img src="data:image/png;base64, ${res}" alt="" class="rounded-circle flex-shrink-0">`
+            `<img src="data:image/png;base64, ${res['image']}" alt="" class="rounded-circle flex-shrink-0">`
+        let feedback = [];
+        feedback = res['feedback'];
+        // 設定成就區域顯示進度條及給予隨機回應
+        let behaviors = ['關心', '零用錢', '稱讚', '餵食', '喝水', '洗澡', '玩耍', '散步', '廁所']
+        let cardTimes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        let achi = document.getElementById('achi')
+        let interactivePartnerFeedback = document.getElementById('interactive-partner-feedback')
 
-        // for (i = 0; i < 16; i++) {
-        //     if (partnerAppearanceArr.toString() == res['appearance'][i].toString()) {
-        //         photo.innerHTML = `<img src='data:image/png;base64, ${res['image'][i]}' alt=''>`
-        //         interactivePartnerPhoto.innerHTML =
-        //             `<img src="data:image/png;base64, ${res['image'][i]}" alt="" class="rounded-circle flex-shrink-0">`
-        //     }
-        // }
+        function progressBarWidth(e) {
+            let n = behaviors.indexOf(e.innerText)
+            let progressBar = document.getElementsByClassName('progress-bar')
+            let achievementProgress = document.getElementsByClassName('achievement-progress')
+            cardTimes[n] += 1
+            achievementProgress[n].style.display = 'block'
+            setTimeout(() => {
+                achievementProgress[n].style.display = 'none'
+            }, 1000)
+            progressBar[n].style.width = cardTimes[n] * 20 + '%'
+            let randomNum = Math.floor(Math.random() * feedback.length)
+            interactivePartnerFeedback.innerHTML += `
+                <div class="d-flex justify-content-between">
+                    <div class="ms-5 position-relative">
+                        <p class="partner-text textFeedback">${feedback[randomNum]}</p>
+                    </div>
+                </div>
+                `
+            // 讓對話框滑到最下面
+            interactivePartnerFeedback.scrollTo(0, interactivePartnerFeedback.scrollHeight)
+            // 隨機回應句子
+            let textFeedback = document.querySelectorAll('.textFeedback')
+            setTimeout(() => {
+                textFeedback[0].classList.remove('textFeedback')
+            }, 1000)
+            let sum = 0
+            cardTimes.forEach((x) => {
+                sum += x
+            })
+            let liveModal = document.querySelector('#liveModal')
+            // 彈跳視窗
+            const modal = new bootstrap.Modal(liveModal)
+            if (sum == 3) {
+                modal.show()
+            }
+        }
+
+        // 偵測點擊互動，觸發function
+        let optionsBehaviors = document.querySelectorAll('.optionsBehaviors')
+        for (let i = 0; i < behaviors.length; i++) {
+            optionsBehaviors[i].addEventListener('click', function () {
+                progressBarWidth(this)
+            })
+        }
+
     })
 }
 
@@ -61,19 +126,19 @@ window.onload = () => {
 
 
 
-let partner_name = localStorage.getItem('partnerName');
-let partner_trait = localStorage.getItem('partnerTrait');
-let partnerTraitArr = JSON.parse(partner_trait);
+// let partner_name = localStorage.getItem('partnerName');
+// let partner_trait = localStorage.getItem('partnerTrait');
+// let partnerTraitArr = JSON.parse(partner_trait);
 // let partner_appearance = localStorage.getItem('partnerAppearance');
 // let partnerAppearanceArr = JSON.parse(partner_appearance);
 // console.log(partnerTraitArr);
-console.log(partnerAppearanceArr);
+// console.log(partnerAppearanceArr);
 
 // 基本資料：姓名
-let partnerName = document.getElementById('partner-name');
-partnerName.innerText = partner_name;
-let interactivePartnerName = document.getElementById('interactive-partner-name')
-interactivePartnerName.innerText = partner_name;
+// let partnerName = document.getElementById('partner-name');
+// partnerName.innerText = partner_name;
+// let interactivePartnerName = document.getElementById('interactive-partner-name')
+// interactivePartnerName.innerText = partner_name;
 // 基本資料：圖片
 // let photoSrc = [
 //     'partners_photo/0000.jpg',
@@ -162,85 +227,85 @@ interactivePartnerName.innerText = partner_name;
 //         `<img src="${photoSrc[15]}" alt="" class="rounded-circle flex-shrink-0">`
 // }
 // 基本資料：特質敘述
-let info = document.getElementById('info');
-info.innerHTML = ` 
-<p class='fs-5 m-0'> ${partner_name} ，是一位${partnerAppearanceArr[0]}風格 、 具有${partnerAppearanceArr[1]} 、 ${partnerAppearanceArr[2]}的${partnerAppearanceArr[3]}寵物！
-    個性${partnerTraitArr[0]} 、 ${partnerTraitArr[1]} 、 ${partnerTraitArr[2]} ，而且喜愛${partnerTraitArr[3]}！ </p>
-`
+// let info = document.getElementById('info');
+// info.innerHTML = ` 
+// <p class='fs-5 m-0'> ${partner_name} ，是一位${partnerAppearanceArr[0]}風格 、 具有${partnerAppearanceArr[1]} 、 ${partnerAppearanceArr[2]}的${partnerAppearanceArr[3]}寵物！
+//     個性${partnerTraitArr[0]} 、 ${partnerTraitArr[1]} 、 ${partnerTraitArr[2]} ，而且喜愛${partnerTraitArr[3]}！ </p>
+// `
 // 特質決定回應句子
-let feedback = [];
-if (partnerTraitArr[0] == '活潑') {
-    feedback[0] = '耶！我最喜歡您！'
-} else {
-    feedback[0] = '謝謝您總是協助我'
-}
-if (partnerTraitArr[1] == '理性') {
-    feedback[1] = '請提供我必要協助'
-} else {
-    feedback[1] = '謝謝您，我好感動'
-}
-if (partnerTraitArr[2] == '謹慎') {
-    feedback[2] = '小心點，別受傷了'
-} else {
-    feedback[2] = '超刺激！我喜歡！'
-}
-if (partnerTraitArr[3] == '戶外活動') {
-    feedback[3] = '帶我去戶外走走吧'
-} else {
-    feedback[3] = '我們一起待在家吧'
-}
+// let feedback = [];
+// if (partnerTraitArr[0] == '活潑') {
+//     feedback[0] = '耶！我最喜歡您！'
+// } else {
+//     feedback[0] = '謝謝您總是協助我'
+// }
+// if (partnerTraitArr[1] == '理性') {
+//     feedback[1] = '請提供我必要協助'
+// } else {
+//     feedback[1] = '謝謝您，我好感動'
+// }
+// if (partnerTraitArr[2] == '謹慎') {
+//     feedback[2] = '小心點，別受傷了'
+// } else {
+//     feedback[2] = '超刺激！我喜歡！'
+// }
+// if (partnerTraitArr[3] == '戶外活動') {
+//     feedback[3] = '帶我去戶外走走吧'
+// } else {
+//     feedback[3] = '我們一起待在家吧'
+// }
 // console.log(feedback);
 
-// 設定成就區域顯示進度條及給予隨機回應
-let behaviors = ['關心', '零用錢', '稱讚', '餵食', '喝水', '洗澡', '玩耍', '散步', '廁所']
-let cardTimes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-let achi = document.getElementById('achi')
-let interactivePartnerFeedback = document.getElementById('interactive-partner-feedback')
+// // 設定成就區域顯示進度條及給予隨機回應
+// let behaviors = ['關心', '零用錢', '稱讚', '餵食', '喝水', '洗澡', '玩耍', '散步', '廁所']
+// let cardTimes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+// let achi = document.getElementById('achi')
+// let interactivePartnerFeedback = document.getElementById('interactive-partner-feedback')
 
-function progressBarWidth(e) {
-    let n = behaviors.indexOf(e.innerText)
-    let progressBar = document.getElementsByClassName('progress-bar')
-    let achievementProgress = document.getElementsByClassName('achievement-progress')
-    cardTimes[n] += 1
-    achievementProgress[n].style.display = 'block'
-    setTimeout(() => {
-        achievementProgress[n].style.display = 'none'
-    }, 1000)
-    progressBar[n].style.width = cardTimes[n] * 20 + '%'
-    let randomNum = Math.floor(Math.random() * feedback.length)
-    interactivePartnerFeedback.innerHTML += `
-                <div class="d-flex justify-content-between">
-                    <div class="ms-5 position-relative">
-                        <p class="partner-text textFeedback">${feedback[randomNum]}</p>
-                    </div>
-                </div>
-                `
-    // 讓對話框滑到最下面
-    interactivePartnerFeedback.scrollTo(0, interactivePartnerFeedback.scrollHeight)
-    // 隨機回應句子
-    let textFeedback = document.querySelectorAll('.textFeedback')
-    setTimeout(() => {
-        textFeedback[0].classList.remove('textFeedback')
-    }, 1000)
-    let sum = 0
-    cardTimes.forEach((x) => {
-        sum += x
-    })
-    let liveModal = document.querySelector('#liveModal')
-    // 彈跳視窗
-    const modal = new bootstrap.Modal(liveModal)
-    if (sum == 3) {
-        modal.show()
-    }
-}
+// function progressBarWidth(e) {
+//     let n = behaviors.indexOf(e.innerText)
+//     let progressBar = document.getElementsByClassName('progress-bar')
+//     let achievementProgress = document.getElementsByClassName('achievement-progress')
+//     cardTimes[n] += 1
+//     achievementProgress[n].style.display = 'block'
+//     setTimeout(() => {
+//         achievementProgress[n].style.display = 'none'
+//     }, 1000)
+//     progressBar[n].style.width = cardTimes[n] * 20 + '%'
+//     let randomNum = Math.floor(Math.random() * feedback.length)
+//     interactivePartnerFeedback.innerHTML += `
+//                 <div class="d-flex justify-content-between">
+//                     <div class="ms-5 position-relative">
+//                         <p class="partner-text textFeedback">${feedback[randomNum]}</p>
+//                     </div>
+//                 </div>
+//                 `
+//     // 讓對話框滑到最下面
+//     interactivePartnerFeedback.scrollTo(0, interactivePartnerFeedback.scrollHeight)
+//     // 隨機回應句子
+//     let textFeedback = document.querySelectorAll('.textFeedback')
+//     setTimeout(() => {
+//         textFeedback[0].classList.remove('textFeedback')
+//     }, 1000)
+//     let sum = 0
+//     cardTimes.forEach((x) => {
+//         sum += x
+//     })
+//     let liveModal = document.querySelector('#liveModal')
+//     // 彈跳視窗
+//     const modal = new bootstrap.Modal(liveModal)
+//     if (sum == 3) {
+//         modal.show()
+//     }
+// }
 
-// 偵測點擊互動，觸發function
-let optionsBehaviors = document.querySelectorAll('.optionsBehaviors')
-for (let i = 0; i < behaviors.length; i++) {
-    optionsBehaviors[i].addEventListener('click', function () {
-        progressBarWidth(this)
-    })
-}
+// // 偵測點擊互動，觸發function
+// let optionsBehaviors = document.querySelectorAll('.optionsBehaviors')
+// for (let i = 0; i < behaviors.length; i++) {
+//     optionsBehaviors[i].addEventListener('click', function () {
+//         progressBarWidth(this)
+//     })
+// }
 
 // 清空localStorage，正式版記得刪掉
 // let btnClear = document.getElementById('clear')
--- js/premiumpage.js
+++ js/premiumpage.js
@@ -1,127 +1,213 @@
 $('.header-page').load('header.html');
-        $('.footer-page').load('footer.html');
+$('.footer-page').load('footer.html');
 
-        // 設定可選購商品
-        let petGoods = []
-        function Goods(name, price, src) {
-            this.name = name;
-            this.price = price;
-            this.image = src;
-        }
-        petGoods[0] = new Goods(`毛絨可愛、大眼睛、小耳朵的可愛寵物`, 40, "./partners_photo/0011.jpg")
-        petGoods[1] = new Goods(`現代機械、小眼睛、大耳朵的強壯寵物`, 30, "./partners_photo/1100.jpg")
-        petGoods[2] = new Goods(`毛絨可愛、小眼睛、大耳朵的可愛寵物`, 50, "./partners_photo/0101.jpg")
-        petGoods[3] = new Goods(`現代機械、大眼睛、小耳朵的強壯寵物`, 40, "./partners_photo/1010.jpg")
-        petGoods[4] = new Goods(`毛絨可愛、大眼睛、大耳朵的可愛寵物`, 50, "./partners_photo/0001.jpg")
+let page = 1;
 
-        // 將單隻購買商品渲染至頁面
-        let petGoodList = document.getElementById('petGoodList')
-        for (i = 0; i < petGoods.length; i++) {
+function getGoods() {
+    fetch(`http://127.0.0.1/AIpartnersShop.php?page=${page}`).then((response) => {
+        return response.json();
+    }).then((res) => {
+        console.log(res);
+        // 渲染初始購買單隻畫面
+        for (i = 0; i < 4; i++) {
             petGoodList.innerHTML +=
                 `
-            <div class='pet-card position-relative col-6 col-sm-4 col-lg-2 my-2'>
-                <form class='cartForm' action='#' method='POST' id='form${i}'>
-                    <img src="${petGoods[i].image}" alt="" class='w-75 pet-card-img m-1'>
-                    <input type="text" hidden value="${petGoods[i].image}" name='goodsImage'>
-                    <p class='text-wrap mx-auto goodsNameText'>${petGoods[i].name}</p>
-                    <input type="text" hidden value="${petGoods[i].name}" name='goodsName'>
-                    <p class='fs-5'>${petGoods[i].price}
-                        <i class='fa-solid fa-dollar-sign darkText'></i>    
-                    </p>
-                    <input type="text" hidden value="${petGoods[i].price}" name='goodsPrice'>
-                    <button class="btn btnMinus" type='button'>
-                        <i class="fa-solid fa-minus"></i>    
-                    </button>
-                    <input type="tel" value='1' id='input${i}' pattern="[0-9]{1}" name='goodsIndex' class='col-4 text-center'>
-                    <button class="btn btnPlus" type='button'>
-                        <i class="fa-solid fa-plus"></i>
-                    </button>
-                    <button type='submit' class='btn btn-outline-secondary col-6 mx-auto my-2 px-0 py-1 submitText'>
-                        <i class="fa-solid fa-cart-plus"></i>
-                        <br>
-                        <small id='cartText'>加入購物車</small>
-                    </button>
-                </form>
-            </div>
-            `
+                    <div class='pet-card position-relative col-6 col-lg-3 my-2'>
+                        <form class='cartForm' action='#' method='POST' id='form${i}'>
+                            <img src="data:image/png;base64, ${res[i].image}" alt="" class='w-75 pet-card-img m-1'>
+                            <input type="text" hidden value="${res[i].image}" name='goodsImage'>
+                            <p class='text-wrap mx-auto goodsNameText'>${res[i].feature}</p>
+                            <input type="text" hidden value="${res[i].feature}" name='goodsName'>
+                            <p class='fs-5'>${res[i].price}
+                                <i class='fa-solid fa-dollar-sign darkText'></i>    
+                            </p>
+                            <input type="text" hidden value="${res[i].price}" name='goodsPrice'>
+                            <button class="btn btnMinus" type='button'>
+                                <i class="fa-solid fa-minus"></i>    
+                            </button>
+                            <input type="tel" value='${res[i].goodIndex}' id='input${i}' pattern="[0-9]{1}" name='goodsIndex' class='col-4 text-center'>
+                            <button class="btn btnPlus" type='button'>
+                                <i class="fa-solid fa-plus"></i>
+                            </button>
+                            <button type='submit' class='btn btn-outline-secondary col-6 mx-auto my-2 px-0 py-1 submitText'>
+                                <i class="fa-solid fa-cart-plus"></i>
+                                <br>
+                                <small id='cartText'>加入購物車</small>
+                            </button>
+                        </form>
+                    </div>
+                    `
         }
-        // 設定加減按鈕控制指定商品數量
-        let btnPlus = document.querySelectorAll('.btnPlus')
-        let btnMinus = document.querySelectorAll('.btnMinus')
+    })
+}
 
-        function cartPlus(e) {
-            let num = parseInt(e.previousElementSibling.value)
-            if (num < 9) {
-                num += 1
-                e.previousElementSibling.value = num
+window.addEventListener('load', function () {
+    getGoods();
+    let pageLink = document.querySelectorAll('.pageLink');
+    let prev = this.document.getElementById('prev');
+    let next = this.document.getElementById('next');
+    // 依照頁數換頁
+    for (i = 0; i < 4; i++) {
+        pageLink[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            page = this.innerText;
+            petGoodList.innerHTML = '';
+            getGoods();
+        })
+    }
+    // 上一頁
+    prev.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (page > 1) {
+            page -= 1;
+            petGoodList.innerHTML = '';
+            getGoods();
+        } else {
+            page = 1;
+            petGoodList.innerHTML = '';
+            getGoods();
+        }
+    })
+    // 下一頁
+    next.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (page < 4) {
+            page += 1;
+            petGoodList.innerHTML = '';
+            getGoods();
+        } else {
+            page = 4;
+            petGoodList.innerHTML = '';
+            getGoods();
+        }
+    })
+})
+
+
+// 設定可選購商品
+let petGoods = []
+
+// function Goods(name, price, src) {
+//     this.name = name;
+//     this.price = price;
+//     this.image = src;
+// }
+// petGoods[0] = new Goods(`毛絨可愛、大眼睛、小耳朵的可愛寵物`, 40, "./partners_photo/0011.jpg")
+// petGoods[1] = new Goods(`現代機械、小眼睛、大耳朵的強壯寵物`, 30, "./partners_photo/1100.jpg")
+// petGoods[2] = new Goods(`毛絨可愛、小眼睛、大耳朵的可愛寵物`, 50, "./partners_photo/0101.jpg")
+// petGoods[3] = new Goods(`現代機械、大眼睛、小耳朵的強壯寵物`, 40, "./partners_photo/1010.jpg")
+// petGoods[4] = new Goods(`毛絨可愛、大眼睛、大耳朵的可愛寵物`, 50, "./partners_photo/0001.jpg")
+
+// 將單隻購買商品渲染至頁面
+// let petGoodList = document.getElementById('petGoodList')
+// for (i = 0; i < petGoods.length; i++) {
+//     petGoodList.innerHTML +=
+//         `
+//             <div class='pet-card position-relative col-6 col-sm-4 col-lg-2 my-2'>
+//                 <form class='cartForm' action='#' method='POST' id='form${i}'>
+//                     <img src="${petGoods[i].image}" alt="" class='w-75 pet-card-img m-1'>
+//                     <input type="text" hidden value="${petGoods[i].image}" name='goodsImage'>
+//                     <p class='text-wrap mx-auto goodsNameText'>${petGoods[i].name}</p>
+//                     <input type="text" hidden value="${petGoods[i].name}" name='goodsName'>
+//                     <p class='fs-5'>${petGoods[i].price}
+//                         <i class='fa-solid fa-dollar-sign darkText'></i>    
+//                     </p>
+//                     <input type="text" hidden value="${petGoods[i].price}" name='goodsPrice'>
+//                     <button class="btn btnMinus" type='button'>
+//                         <i class="fa-solid fa-minus"></i>    
+//                     </button>
+//                     <input type="tel" value='1' id='input${i}' pattern="[0-9]{1}" name='goodsIndex' class='col-4 text-center'>
+//                     <button class="btn btnPlus" type='button'>
+//                         <i class="fa-solid fa-plus"></i>
+//                     </button>
+//                     <button type='submit' class='btn btn-outline-secondary col-6 mx-auto my-2 px-0 py-1 submitText'>
+//                         <i class="fa-solid fa-cart-plus"></i>
+//                         <br>
+//                         <small id='cartText'>加入購物車</small>
+//                     </button>
+//                 </form>
+//             </div>
+//             `
+// }
+
+// 設定加減按鈕控制指定商品數量
+let btnPlus = document.querySelectorAll('.btnPlus')
+let btnMinus = document.querySelectorAll('.btnMinus')
+
+function cartPlus(e) {
+    let num = parseInt(e.previousElementSibling.value)
+    if (num < 9) {
+        num += 1
+        e.previousElementSibling.value = num
+    }
+}
+
+function cartMinus(e) {
+    let num = parseInt(e.nextElementSibling.value)
+    if (num > 0) {
+        num -= 1
+        e.nextElementSibling.value = num
+    }
+}
+for (i = 0; i < petGoods.length; i++) {
+    btnPlus[i].addEventListener('click', function () {
+        cartPlus(this)
+    })
+}
+for (i = 0; i < petGoods.length; i++) {
+    btnMinus[i].addEventListener('click', function () {
+        cartMinus(this)
+    })
+}
+
+// 設定購買成功彈跳視窗
+let cartToast = document.getElementById('cartToast')
+const cToast = new bootstrap.Toast(cartToast)
+let guideToast = document.getElementById('guideToast')
+const gToast = new bootstrap.Toast(guideToast)
+// 透過表單抓取「加入購物車」商品名稱、價錢、數目資料
+let shoppingList = []
+let cartForm = document.querySelectorAll('.cartForm')
+for (i = 0; i < petGoods.length; i++) {
+    cartForm[i].addEventListener('submit', function (event) {
+        event.preventDefault(); // 暫停網頁跳轉
+        for (j = 0; j < petGoods.length; j++) {
+            if (this.id == `form${j}`) {
+                shoppingList[j] = Array.from(new FormData(document.getElementById(this
+                    .id))) // Array.from() 將 FormData 資料轉為可以存進 localStorage 的格式
+                // console.log(shoppingList)
             }
         }
-        function cartMinus(e) {
-            let num = parseInt(e.nextElementSibling.value)
-            if (num > 0) {
-                num -= 1
-                e.nextElementSibling.value = num
+        localStorage.setItem('shoppingList', JSON.stringify(shoppingList)) // 將已購買商品資訊存入儲存空間
+        let sum = 0
+        for (s = 0; s < shoppingList.length; s++) {
+            if (shoppingList[s]) {
+                sum += parseInt(shoppingList[s][2][1] * shoppingList[s][3][1])
+                if (sum > 199) {
+                    gToast.show() // 引導訂閱月費制
+                } else {
+                    cToast.show() // 成功加入購物車
+                }
             }
         }
-        for (i = 0; i < petGoods.length; i++) {
-            btnPlus[i].addEventListener('click', function () {
-                cartPlus(this)
-            })
-        }
-        for (i = 0; i < petGoods.length; i++) {
-            btnMinus[i].addEventListener('click', function () {
-                cartMinus(this)
-            })
-        }
 
-        // 設定購買成功彈跳視窗
-        let cartToast = document.getElementById('cartToast')
-        const cToast = new bootstrap.Toast(cartToast)
-        let guideToast = document.getElementById('guideToast')
-        const gToast = new bootstrap.Toast(guideToast)
-        // 透過表單抓取「加入購物車」商品名稱、價錢、數目資料
-        let shoppingList = []
-        let cartForm = document.querySelectorAll('.cartForm')
-        for (i = 0; i < petGoods.length; i++) {
-            cartForm[i].addEventListener('submit', function (event) {
-                event.preventDefault(); // 暫停網頁跳轉
-                for (j = 0; j < petGoods.length; j++) {
-                    if (this.id == `form${j}`) {
-                        shoppingList[j] = Array.from(new FormData(document.getElementById(this
-                            .id))) // Array.from() 將 FormData 資料轉為可以存進 localStorage 的格式
-                        // console.log(shoppingList)
-                    }
-                }
-                localStorage.setItem('shoppingList', JSON.stringify(shoppingList)) // 將已購買商品資訊存入儲存空間
-                let sum = 0
-                for (s = 0; s < shoppingList.length; s++) {
-                    if (shoppingList[s]) {
-                        sum += parseInt(shoppingList[s][2][1] * shoppingList[s][3][1])
-                        if (sum > 199) {
-                            gToast.show() // 引導訂閱月費制
-                        }else{
-                            cToast.show() // 成功加入購物車
-                        }
-                    }
-                }
+    })
+}
 
-            })
-        }
+// 引導訂閱月費制成功，清除購物車資料
+let guideToastAgain = document.getElementById('guideToastAgain')
+guideToastAgain.addEventListener('click', function () {
+    localStorage.setItem('shoppingList', null)
+    window.open('./premiumpage.html', '_self')
+})
 
-        // 引導訂閱月費制成功，清除購物車資料
-        let guideToastAgain = document.getElementById('guideToastAgain')
-        guideToastAgain.addEventListener('click',function(){
-            localStorage.setItem('shoppingList',null)
-            window.open('./premiumpage.html','_self')
-        })
-
-        // 設定訂閱成功彈跳視窗
-        let monthForm = document.getElementById('monthForm')
-        let monthToast = document.getElementById('monthToast')
-        const mToast = new bootstrap.Toast(monthToast)
-        monthForm.addEventListener('submit', function (event) {
-            event.preventDefault()
-            let monthPlan = Array.from(new FormData(monthForm))
-            localStorage.setItem('monthPlan', JSON.stringify(monthPlan))
-            mToast.show()
-        })
+// 設定訂閱成功彈跳視窗
+let monthForm = document.getElementById('monthForm')
+let monthToast = document.getElementById('monthToast')
+const mToast = new bootstrap.Toast(monthToast)
+monthForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    let monthPlan = Array.from(new FormData(monthForm))
+    localStorage.setItem('monthPlan', JSON.stringify(monthPlan))
+    mToast.show()
+})
